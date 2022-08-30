ad78a43bbace3ee0b2bc40a62713217584fc7f69
Kamil Kasiak <kasiakkamil@gmail.com>
2022-08-30 16:03:17 +0200

    correct code and file possition for deplyoment

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,14 @@
 import express from "express"
+import mongoose from "mongoose"
 import dotenv from "dotenv"
 dotenv.config()
 import cors from "cors"
-import mongoose from "mongoose"
 import router from "./routes/tracks.js"
 import userRouter from "./routes/user.js"
-import connectDB from './config/db.js';
-const port = process.env.PORT || 4000;
 
-// CONNECT TO DB
-connectDB();
-
+const url = process.env.MONGO_URI;
 const app = express();
-
+const port = process.env.PORT || 4000;
 
 // MIDDLEWARE
 app.use(express.json())
@@ -27,7 +23,7 @@ app.use("/api/tracks",router)
 app.use("/api/user",userRouter)
 
 //SERVE Frontend on production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV == "production") {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
     app.get("*", (req,res) => {
@@ -39,11 +35,19 @@ if (process.env.NODE_ENV === "production") {
     app.get('/', (req,res) => res.send("Please set env to production mode"))
 }
 
-
-// LISTEN for request only if connected to DB
-app.listen(port, () => {
-    console.log(`Connected to Database and listening at port ${port}`)
-})
-
+// CONNECT TO DB
+mongoose.connect(url, {  useUnifiedTopology:true,
+    useNewUrlParser: true,
+    useCreateIndex: true})
+    .then(() => {
+        // LISTEN for request only if connected to DB
+        app.listen(port, () => {
+            console.log(`Connected to Database and listening at port ${port}`)
+        })
+    })
+    .catch( (err) => {
+        console.log(err)
+        process.exit(1)
+    })
 
 
