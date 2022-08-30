81620c3a065caeb5007103d0aa4c83508a31e6d4
Kamil Kasiak <kasiakkamil@gmail.com>
2022-08-30 19:32:41 +0200

    change fetch in useLogin and useSignup

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -9,8 +9,8 @@ export const useSignup = () => {
     const signup = async (email, password) => {
         setIsLoading(true)
         setError(null)
-
-        const response = await fetch("http://localhost:4000/api/user/signup", {
+            //http://localhost:4000
+        const response = await fetch("/api/user/signup", {
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({email, password})
